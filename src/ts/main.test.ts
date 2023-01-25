02b73b6a737470d0031d13235d8fe1ad22de73bd
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-01-25 09:59:38 +0100

    add coverage map

--- src/ts/main.test.ts
+++ src/ts/main.test.ts
@@ -1,8 +1,11 @@
 import { createNewTodo, createHtml, toggleTodo, displayError, clearTodos } from "./main";
+import { Todo } from "./models/Todo";
 
 /*******| Function createNewTodo |*******/ 
 
+
 /*******| Function createHtml |*******/ 
+
 
 /*******| Function toggleTodo |*******/ 
 
