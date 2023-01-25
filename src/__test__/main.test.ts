a4696290c33c4fa2b25d3d07f5da095e4ccbfa27
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-01-25 12:42:23 +0100

    create tests for functions.ts

---
+++ src/__test__/main.test.ts
@@ -0,0 +1,5 @@
+// Arrange - skapa förutsättningar för att kunna anropa vår funktion
+// Act - Anropar funktionen som skall testas
+// Assert - Kontroll av förändrade värden
+
+/******* Function createNewTodo ********/
