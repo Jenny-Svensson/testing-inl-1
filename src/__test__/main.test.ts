ff93f719ac3add8016c057648c7a5c53ff4ee26c
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-01-26 10:26:00 +0100

    test the function createNewTodo

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,19 +1,35 @@
 /**
- * @ jest-environment jsdom
+* @jest-environment jsdom
 */
 
-import { createHtml, createNewTodo, toggleTodo, displayError } from "../ts/main";
+import { createHtml, createNewTodo, toggleTodo, displayError, clearTodos } from "../ts/main";
 import { Todo } from "../ts/models/Todo";
 
 // Arrange - skapa förutsättningar för att kunna anropa vår funktion
 // Act - Anropar funktionen som skall testas
 // Assert - Kontroll av förändrade värden
 
-/******* Function createNewTodo IF ********/
+/* 
 
 
-/******* Function createNewTodo ELSE ********/
+ */
 
+/******* Function createNewTodo ********/
+test('should createHtml if result is success and displayerror if result is error ', () => {
+    // Arrange
+    document.body.innerHTML = `
+    <ul id="todos" class="todo"></ul>
+    `;
+    let todos: Todo[] = [
+        {text: 'World', done: true},
+    ];
+    // Act
+    createNewTodo('Test', todos);
+    // Assert
+    let result = document.getElementById('todos');
+    expect(result).not.toBeNull();
+    expect(result?.innerHTML).not.toBe('Test');
+});
 
 /******* Function createHtml ********/
 
@@ -22,6 +38,15 @@ import { Todo } from "../ts/models/Todo";
 
 
 /******* Function displayError ********/
-
+test('should add class "show" if its true and display error text ', () => {
+    // Arrange
+    let errorText = 'An error has occured'
+    document.body.innerHTML = `<div id="error" class"error"></div>`;
+    // Act
+    displayError(errorText, true);
+    // Assert
+    let result = document.getElementById('error')?.innerHTML
+    expect(result).toBe(errorText);
+})
 
 /******* Function clearTodos ********/
